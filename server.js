be2d191e9eb1226a4d243f65554bd56bef0e0bc9
shaikhshehzad <shehzadali3113@gmail.com>
2023-05-25 23:03:55 +0530

    pushing dummy assets

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require("path");
+const publicPath = path.join(__dirname, "assets");
 
 app.get('/', (req, res) => {
   res.send('<h1>Travel Agency QA1 enviornment is up now </h1>');
+});
+
+app.get('/dummy-assets', (req, res) => {
+  res.sendFile(`${publicPath}/dummy.png`);
 });
 
 app.listen(port, () => {
